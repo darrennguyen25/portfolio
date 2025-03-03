bb59d8f301f28982577374d07a95fca9cc3ba6f6
Darren Nguyen <dnguyenbusiness616@gmail.com>
2025-03-03 03:26:30 -0800

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel/static";
@@ -13,15 +12,7 @@ const prettyCodeOptions = {
 };
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    mdx({
-      syntaxHighlight: false,
-      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
-      extendMarkdownConfig: true,
-    }),
-    icon(),
-  ],
+  integrations: [tailwind(), icon()],
   output: "static",
   site: "https://darrennguyen25-portfolio.vercel.app",
   adapter: vercel({
